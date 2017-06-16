23904756d3250da4a2a4ba7ec596cdf2986d111f
florsecchi <florencia.secchi@gmail.com>
2017-06-16 18:55:10 -0300

    responsive-mediaquery,menu hamburguesa, Jquery menu desplegable

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+$('#hambur').on('mouseenter', function(){
+	 $('#menumob').css({display: "block"});
+   });
+
+$('#hambur').on('mouseleave', function(){
+	 $('#menumob').css({display: "none"});
+   });
+});
